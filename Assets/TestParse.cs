cf13b8efcb9e829cedd853390b229fea32be4f2a
William Crawford <wccrawford@gmail.com>
2012-12-02 18:47:58 -0500

    Updated sample project to use new component that handles most of the ugly loading work.

--- Assets/TestParse.cs
+++ Assets/TestParse.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections;
-using Kelahn.OBJ;
 
 public class TestParse : MonoBehaviour {
 	public Material testMaterial;
@@ -65,52 +64,11 @@ public class TestParse : MonoBehaviour {
 "f 1/17 2/18 3/19 4/20\n" +
 "f 8/10 7/9 6/21 5/22\n" +
 "";
-		Model obj = new Model(fileContents);
-		
 		GameObject newObject = new GameObject("newObject");
-		newObject.AddComponent("MeshFilter");
-		newObject.AddComponent("MeshRenderer");
-		
-		Mesh mesh = newObject.GetComponent<MeshFilter>().mesh;
-		mesh.Clear();
-		
-		float[][] objVerts = obj.getVertices();
-		Vector3[] vertices = new Vector3[objVerts.Length];
-		for(int index = 0; index < objVerts.Length; index++) {
-			vertices[index] = new Vector3((0 - objVerts[index][0]), (objVerts[index][1]), (objVerts[index][2]));
-		}
-		mesh.vertices = vertices;
-		
-		float[][] objUVs = obj.getUVs();
-		if((objUVs != null) && (objUVs.Length > 0)) {
-			Vector2[] uv = new Vector2[objUVs.Length];
-			for(int index = 0; index < objUVs.Length; index++) {
-				uv[index] = new Vector2((objUVs[index][0]), (objUVs[index][1]));
-			}
-			mesh.uv = uv;
-		}
-		
-		float[][] objNorms = obj.getNormals();
-		if((objNorms != null) && (objNorms.Length > 0)) {
-			Vector3[] normals = new Vector3[objNorms.Length];
-			for(int index = 0; index < objNorms.Length; index++) {
-				Debug.Log(objNorms[index]);
-				normals[index] = new Vector3((0 - objNorms[index][0]), (objNorms[index][1]), (objNorms[index][2]));
-			}
-			mesh.normals = normals;
-		} else {
-			mesh.RecalculateNormals();
-		}
-		
-		int[][] objFaces = obj.getFaces();
-		int[] faces = new int[objFaces.Length * 3];
-		for(int index = 0; index < objFaces.Length; index++) {
-			faces[(index * 3) + 0] = objFaces[index][2];
-			faces[(index * 3) + 1] = objFaces[index][1];
-			faces[(index * 3) + 2] = objFaces[index][0];
-		}
-		mesh.triangles = faces;
-		
+		newObject.AddComponent("ObjectLoader");
+		ObjectLoader objLoader = newObject.GetComponent<ObjectLoader>();
+		objLoader.CreateMesh(fileContents);
+				
 		newObject.GetComponent<MeshRenderer>().material = testMaterial;
 	}
 }
